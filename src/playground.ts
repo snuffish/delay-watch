dd0053fa4cbaf7d3b6a514acd64c813d28bdcf87
Christoffer Engman <snuffish90@gmail.com>
2026-01-07 16:39:38 +0100

    wip

--- src/playground.ts
+++ src/playground.ts
@@ -1,8 +1,95 @@
-import { Trip } from "./ScanLocation";
-import { getTrafficInfo, REQUEST_TYPE } from "./Utils/traffic"
+import {FORMAT, getDate} from "./Utils/date";
 
-    ; (async () => {
-        const data = await getTrafficInfo(REQUEST_TYPE.TRAIN, 81039)
-        const trip = new Trip(data)
-        console.log(trip)
-    })()
+import stations from './resources/stations.json'
+
+type Train = {
+    trainNumber: string
+    operation: string
+    station: string
+    transportType: string
+}
+
+type StationInfo = {
+    locationId: string,
+    locationName: string
+    arrivalConnections: [
+        Train
+    ],
+    departureConnections: [
+        Train
+    ]
+}
+
+type Station = {
+    name: string
+    locationCode: string
+    delayed: boolean
+}
+
+type TrainStationInfo = Pick<Station, 'delayed'> & {
+    currentTime: string
+    originalTime: string
+}
+
+type TrainInfo = Pick<Train, 'trainNumber' | 'transportType'> & {
+    stations: [
+        Station & {
+            departure?: TrainStationInfo
+            arrival?: TrainStationInfo
+        }
+    ]
+}
+
+const headers = {
+    'ocp-apim-subscription-key': '39296c1a13304493b44236e1bcb7f544'
+}
+
+const getTrainInfo = async (trainNumber: number) => {
+    const req = await fetch(
+        `https://prod-api.adp.sj.se/public/trafficinfo-api/v2/rest/train-routes?transportId=${trainNumber}&lang=sv-SE&date=2026-01-07`, {
+            method: 'GET',
+            headers
+        }
+    )
+    
+    const data = await req.json() as TrainInfo
+    
+    return {
+        ...data
+    }
+}
+
+const getStationInfo = async (locationCode: string) => {
+    const req = await fetch(
+        `https://prod-api.adp.sj.se/public/trafficinfo-api/v2/rest/connections?location=${locationCode}&date=2026-01-07&lang=sv-SE&allDay=true`, {
+            method: 'GET',
+            headers
+        }
+    )
+
+    const data = await req.json() as StationInfo;
+
+    return {
+        trainNumbers: data.departureConnections.map(item => parseInt(item.trainNumber)),
+        ...data
+    }
+};
+
+;(async () => {
+    const info = await getStationInfo('G')
+    const trainNumbers = info.trainNumbers
+    
+    // console.log(trainNumbers)
+    
+    const trainInfo = await getTrainInfo(3427)
+    const delayedStations = trainInfo.stations.filter(station => station.delayed)
+    
+    const times = delayedStations.map(station => ({
+        station: station.name,
+        originalTime: station.departure?.originalTime,
+        realTime: station.departure?.currentTime
+    }))
+
+    
+    console.log(times)
+})()
